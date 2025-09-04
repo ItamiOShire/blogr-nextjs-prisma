d985c464599006251abab15bb527a982ced1dc1b
Adam <a.niewczas01@gmail.com>
2025-09-04 12:57:54 +0200

    fix: getSession replaced with getServerSession due to constant 'null session object' response

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,8 +1,22 @@
 import { getSession } from 'next-auth/react';
+import { getServerSession } from "next-auth/next";
 import prisma from '../../../lib/prisma';
+import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handle(req, res) {
-    const session = await getSession({ req });
+
+    /* doesn't work cause idk why
+    doesn't get fuill context of session and returns null, even when logged in ( api/auth/session returns proper object with proper user details) */
+
+    //const session = await getSession({req});
+
+    const session = await getServerSession(req, res, authOptions);
+
+    console.log(session);
+
+    if (!session || !session.user) {
+        return res.status(401).json({ error: "Unauthorized" });
+    }
 
     const { title, content } = req.body;
 
@@ -12,7 +26,7 @@ export default async function handle(req, res) {
             content: content,
             author: { 
                 connect: {
-                    email: session?.user?.email
+                    email: session.user.email
                 }
             }
         }
